49049248e6daad77f84eaa579b0dbfc140ca3626
alangibson <alan.gibson@gmail.com>
2023-12-26 12:11:45 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'pykwb',
-    version = '0.1.0',
+    version = '0.1.1',
     packages = ['pykwb'],
     install_requires = ['pyserial>=3.0.1'],
     description = 'KWB library, for inclusion into HomeAssistant',
